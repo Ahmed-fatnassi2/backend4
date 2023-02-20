f27298c5e334cba9a4309796c5badf24a3c86f47
Ahmed-fatnassi2 <98342796+Ahmed-fatnassi2@users.noreply.github.com>
2023-02-21 00:13:45 +0100

    Add files via upload

---
+++ api/produit.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const con = require("../server");
+const app = express();
+
+
+app.get("/", (req, resp) => {
+  con.query("select * from product", (err, result) => {
+    if (err) { resp.send("error in api") }
+    else { resp.send(result) }
+  })
+});
+ app.post("/",(req,resp)=>{
+  const data={name:req.name,price:req.price,id_categorie:req.id_categorie,src:req.src};
+  con.query("INSERT INTO product SET?",data,(error,results,fields)=>{
+    if(error)throw error;
+    resp.send(results)
+  })
+ })
+ app.delete("/:id",(req,resp)=>{
+  con.query("DELETE FROM produit WHERE id="+req.params.id,(error,results)=>{
+    if(error)throw error;
+    resp.send(results)})
+  })
+
+
+
+app.listen("5000")
